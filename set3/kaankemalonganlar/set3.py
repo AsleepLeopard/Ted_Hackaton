274b6fb9b750354f11f4f403e504c05ed1d3fc58
kkonganlar <kkonganlar@gmail.com>
2025-04-21 11:53:06 +0300

    Add files via upload

---
+++ set3/kaankemalonganlar/set3.py
@@ -0,0 +1,17 @@
+import turtle
+import time
+
+turtle1 = turtle
+turtle1.pencolor("red")
+turtle1.speed(1)
+turtle1.pendown()
+turtle1.forward(200)
+turtle1.right(150)
+turtle1.forward(200)
+turtle1.right(140)
+turtle1.forward(200)
+turtle1.right(140)
+turtle1.forward(200)
+turtle1.right(140)
+turtle1.forward(200)
+time.sleep(100)
